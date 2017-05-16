cbb230901eb94c01b00c13c54a1ec2a9f139f674
tiberio40 <frenando40@hotmail.com>
2017-05-16 18:10:39 -0500

    Creación IU
    Primera parte

---
+++ betaImagenes/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Media.Capture;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0xc0a
+
+namespace betaImagenes
+{
+    /// <summary>
+    /// Página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        StorageFile imagenCamara;
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        public async System.Threading.Tasks.Task cameraAsync()
+        {
+            CameraCaptureUI dialog = new CameraCaptureUI();
+            Size aspectRatio = new Size(16, 9);
+            dialog.PhotoSettings.CroppedAspectRatio = aspectRatio;
+
+            imagenCamara = await dialog.CaptureFileAsync(CameraCaptureUIMode.Photo);
+        }
+
+
+        private async void capturaImagen_Click(object sender, RoutedEventArgs e)
+        {
+            await cameraAsync();
+        }
+    }
+}
